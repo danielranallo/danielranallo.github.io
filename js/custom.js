5ffa10895a1f100e91a10ecd1e90dba5e8256cdf
Daniel Ranallo <daniel@danielranallo.com>
2016-09-13 20:31:07 -0600

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,230 @@
+$(document).ready(function () {
+    /***************** Navbar-Collapse ******************/
+
+    $(window).scroll(function () {
+        if ($(".navbar").offset().top > 50) {
+            $(".navbar-fixed-top").addClass("top-nav-collapse");
+        } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        }
+    });
+
+    /***************** Page Scroll ******************/
+
+    $(function () {
+        $('a.page-scroll').bind('click', function (event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1500, 'easeInOutExpo');
+            event.preventDefault();
+        });
+    });
+
+    /***************** Scroll Spy ******************/
+
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    })
+
+    /***************** Owl Carousel ******************/
+
+    $("#owl-hero").owlCarousel({
+
+        navigation: true, // Show next and prev buttons
+        slideSpeed: 300,
+        paginationSpeed: 400,
+        singleItem: true,
+        transitionStyle: "fadeUp",
+        autoPlay: true,
+        navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"]
+
+    });
+
+
+    /***************** Full Width Slide ******************/
+
+    var slideHeight = $(window).height();
+
+    $('#owl-hero .item').css('height', slideHeight);
+
+    $(window).resize(function () {
+        $('#owl-hero .item').css('height', slideHeight);
+    });
+    /***************** Owl Carousel Testimonials ******************/
+
+    $("#owl-testi").owlCarousel({
+
+        navigation: false, // Show next and prev buttons
+        paginationSpeed: 400,
+        singleItem: true,
+        transitionStyle: "backSlide",
+        autoPlay: true
+
+    });
+    /***************** Countdown ******************/
+
+    $('#fun-facts').bind('inview', function (event, visible, visiblePartX, visiblePartY) {
+        if (visible) {
+            $(this).find('.timer').each(function () {
+                var $this = $(this);
+                $({
+                    Counter: 0
+                }).animate({
+                    Counter: $this.text()
+                }, {
+                    duration: 2000,
+                    easing: 'swing',
+                    step: function () {
+                        $this.text(Math.ceil(this.Counter));
+                    }
+                });
+            });
+            $(this).unbind('inview');
+        }
+    });
+
+    /***************** Wow.js ******************/
+    
+    new WOW().init();
+    
+    /***************** Preloader ******************/
+    
+    var preloader = $('.preloader');
+    $(window).load(function () {
+        preloader.remove();
+    });
+});
+
+
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD
+        define(['jquery'], factory);
+    } else if (typeof exports === 'object') {
+        // CommonJS
+        factory(require('jquery'));
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
+}(function ($) {
+    var CountTo = function (element, options) {
+        this.$element = $(element);
+        this.options  = $.extend({}, CountTo.DEFAULTS, this.dataOptions(), options);
+        this.init();
+    };
+
+    CountTo.DEFAULTS = {
+        from: 0,               // the number the element should start at
+        to: 0,                 // the number the element should end at
+        speed: 1000,           // how long it should take to count between the target numbers
+        refreshInterval: 100,  // how often the element should be updated
+        decimals: 0,           // the number of decimal places to show
+        formatter: formatter,  // handler for formatting the value before rendering
+        onUpdate: null,        // callback method for every time the element is updated
+        onComplete: null       // callback method for when the element finishes updating
+    };
+
+    CountTo.prototype.init = function () {
+        this.value     = this.options.from;
+        this.loops     = Math.ceil(this.options.speed / this.options.refreshInterval);
+        this.loopCount = 0;
+        this.increment = (this.options.to - this.options.from) / this.loops;
+    };
+
+    CountTo.prototype.dataOptions = function () {
+        var options = {
+            from:            this.$element.data('from'),
+            to:              this.$element.data('to'),
+            speed:           this.$element.data('speed'),
+            refreshInterval: this.$element.data('refresh-interval'),
+            decimals:        this.$element.data('decimals')
+        };
+
+        var keys = Object.keys(options);
+
+        for (var i in keys) {
+            var key = keys[i];
+
+            if (typeof(options[key]) === 'undefined') {
+                delete options[key];
+            }
+        }
+
+        return options;
+    };
+
+    CountTo.prototype.update = function () {
+        this.value += this.increment;
+        this.loopCount++;
+
+        this.render();
+
+        if (typeof(this.options.onUpdate) == 'function') {
+            this.options.onUpdate.call(this.$element, this.value);
+        }
+
+        if (this.loopCount >= this.loops) {
+            clearInterval(this.interval);
+            this.value = this.options.to;
+
+            if (typeof(this.options.onComplete) == 'function') {
+                this.options.onComplete.call(this.$element, this.value);
+            }
+        }
+    };
+
+    CountTo.prototype.render = function () {
+        var formattedValue = this.options.formatter.call(this.$element, this.value, this.options);
+        this.$element.text(formattedValue);
+    };
+
+    CountTo.prototype.restart = function () {
+        this.stop();
+        this.init();
+        this.start();
+    };
+
+    CountTo.prototype.start = function () {
+        this.stop();
+        this.render();
+        this.interval = setInterval(this.update.bind(this), this.options.refreshInterval);
+    };
+
+    CountTo.prototype.stop = function () {
+        if (this.interval) {
+            clearInterval(this.interval);
+        }
+    };
+
+    CountTo.prototype.toggle = function () {
+        if (this.interval) {
+            this.stop();
+        } else {
+            this.start();
+        }
+    };
+
+    function formatter(value, options) {
+        return value.toFixed(options.decimals);
+    }
+
+    $.fn.countTo = function (option) {
+        return this.each(function () {
+            var $this   = $(this);
+            var data    = $this.data('countTo');
+            var init    = !data || typeof(option) === 'object';
+            var options = typeof(option) === 'object' ? option : {};
+            var method  = typeof(option) === 'string' ? option : 'start';
+
+            if (init) {
+                if (data) data.stop();
+                $this.data('countTo', data = new CountTo(this, options));
+            }
+
+            data[method].call(data);
+        });
+    };
+}));
